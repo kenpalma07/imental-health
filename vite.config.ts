2f3d23ebd27bc90ca47eefc21d006ac332e9c143
Seigi Takada <choji.takada@gmail.com>
2025-04-10 14:15:35 +0800

    Change js obfuscator

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
-import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+//import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+import { viteObfuscateFile } from "vite-plugin-obfuscator";
 
 export default defineConfig({
     plugins: [
@@ -14,17 +15,35 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
-        obfuscatorPlugin({
-            //include: ["src/path/to/file.js", "path/anyjs/**/*.js", /foo.js$/],
-            exclude: [/node_modules/],
-            apply: "build",
-            debugger: true,
-            options: {
-              // your javascript-obfuscator options
-              debugProtection: false,
-              disableConsoleOutput: true,
-              // ...  [See more options](https://github.com/javascript-obfuscator/javascript-obfuscator)
-            },
+        viteObfuscateFile({
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+            debugProtection: false,
+            debugProtectionInterval: 0,
+            disableConsoleOutput: true,
+            identifierNamesGenerator: "hexadecimal",
+            log: false,
+            numbersToExpressions: true,
+            renameGlobals: false,
+            selfDefending: false,
+            simplify: true,
+            splitStrings: true,
+            splitStringsChunkLength: 10,
+            stringArray: true,
+            stringArrayEncoding: ["base64"],
+            stringArrayIndexShift: true,
+            stringArrayRotate: true,
+            stringArrayShuffle: true,
+            stringArrayWrappersCount: 2,
+            stringArrayWrappersChainedCalls: true,
+            stringArrayWrappersParametersMaxCount: 4,
+            stringArrayWrappersType: "function",
+            stringArrayThreshold: 0.75,
+            transformObjectKeys: true,
+            unicodeEscapeSequence: false,
         }),
     ],
     esbuild: {
